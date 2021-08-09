452114a8002c32e82175efd1af819c16985e4611
João Paulo Affini Ximenes <joaoaffini@gmail.com>
2021-08-09 16:01:20 -0300

    Buscando dados de worker utilizando restTemplate

--- hr-payroll/src/main/java/com/jp/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/jp/hrpayroll/service/PaymentService.java
@@ -1,13 +1,32 @@
 package com.jp.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.jp.hrpayroll.entity.Payment;
+import com.jp.hrpayroll.entity.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", workerId.toString());
+		
+		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, pathVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
