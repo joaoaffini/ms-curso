ccd2ae8a2fff98292b386e7acb53a3e5dffda935
João Paulo Affini Ximenes <joaoaffini@gmail.com>
2021-08-11 23:59:17 -0300

    hr-user mapeamento das tabelas

---
+++ hr-user/src/main/java/com/jp/hruser/entity/Role.java
@@ -0,0 +1,53 @@
+package com.jp.hruser.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+	
+//	@ManyToMany
+//	private User users;
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(roleName, other.roleName);
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(roleName);
+	}
+	
+	
+
+}
